8a910cddc5492fc22d32babfc96e0c495119cbd4
EdivarAraujo <edivar23araujo@gmail.com>
2022-08-05 07:46:53 -0300

    principal

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Bem vindo a Tijuca Alimentos</title>
+</head>
+<body>
+  <div id="barra-topo">Tijuca Alimentos</div> <!--fim da div barra-topo-->
+  <div id="menu">
+    <ul class="">
+      <li>Principal</li>
+      <li>Futebol</li>
+      <li>Ciclismo</li>
+      <li>Corrida</li>
+      <li>Tecnologia</li>
+      <li>Contato</li>
+    </ul>
+  </div> <!--Fim da div menu-->
+</body>
+</html>
